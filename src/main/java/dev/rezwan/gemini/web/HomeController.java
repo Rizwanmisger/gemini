ab95ceb31708b118f7434b4ef1abd2cb6e17d535
rizwanmisger <rizwanmisger.20@gmail.com>
2025-08-12 12:09:17 +0530

    Refactor to use thymeleaf

--- src/main/java/dev/rezwan/gemini/web/HomeController.java
+++ src/main/java/dev/rezwan/gemini/web/HomeController.java
@@ -1,16 +1,26 @@
 package dev.rezwan.gemini.web;
 
-import gg.jte.models.runtime.JteModel;
+import dev.rezwan.gemini.models.Template;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public JteModel index(@AuthenticationPrincipal OAuth2User principal) {
-        return new JteModel("index.jte").with("principal", principal);
+    public String index(@AuthenticationPrincipal OAuth2User principal, Model model) {
+        List<Template> templates = new ArrayList<>();
+        for (int i = 1; i <= 9; i++) {
+            templates.add(new Template("Template " + i, "A short description of the template.", i));
+        }
+        model.addAttribute("principal", principal);
+        model.addAttribute("templates", templates);
+        return "index";
     }
 }
